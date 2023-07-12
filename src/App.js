9c8f27fcee09b0dc7ff68b3d61c0b40b6d5b6134
Dewkong <kryniu113@gmail.com>
2023-07-13 00:26:31 +0200

    Add first food element (table). Add Robert

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import robert from './robert-lewandowski.png';
 import './App.css';
+
+const weekData = [
+  { day: "Monday", food: "China Bar" },
+  { day: "Tuesday", food: "Sevi Oriental" },
+  { day: "Wednesday", food: "Hindus" },
+  { day: "Thursday", food: "Variety (Bonarka)" },
+  { day: "Friday", food: "Variety (Bonarka)" },
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-        <p>This is <code>Daily Krystian Status</code></p>
+        <h1>Daily Krystian Status</h1>
+        <img src={robert} className="App-logo" alt="robert" />
+        <p className='quote'>Every day is a soup day ~ wonim</p>
+        <table>
+          <tr>
+            <th>Week day</th>
+            <th>Food</th>
+          </tr>
+          {
+            weekData.map((val, key) => {
+              return (
+                  <tr key={key}>
+                      <td>{val.day}</td>
+                      <td>{val.food}</td>
+                  </tr>
+              )
+          })}
+        </table>
       </header>
     </div>
   );
